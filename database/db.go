fa52a26fcfac52e9c53e96e6e04db6c2eb982390
MikeMwita <mwitamike8@gmail.com>
2023-09-14 15:51:49 +0300

    latest code

--- database/db.go
+++ database/db.go
@@ -41,34 +41,34 @@ var (
 func ConnectDatabase() *gorm.DB {
 	// Read environment variables or provide default values for local development
 	dbHost := os.Getenv("DATABASE_HOST")
-	if dbHost == "" {
-		// Set a default value for local development
-		dbHost = "filtronicdb"
-	}
+	//if dbHost == "" {
+	//	// Set a default value for local development
+	//	dbHost = "filtronicdb"
+	//}
 
 	dbUser := os.Getenv("DATABASE_USER")
-	if dbUser == "" {
-		// Set a default value for local development
-		dbUser = "filtronic"
-	}
+	//if dbUser == "" {
+	//	// Set a default value for local development
+	//	dbUser = "filtronic"
+	//}
 
 	dbPassword := os.Getenv("DATABASE_PASSWORD")
-	if dbPassword == "" {
-		// Set a default value for local development
-		dbPassword = "secret"
-	}
+	//if dbPassword == "" {
+	//	// Set a default value for local development
+	//	dbPassword = "secret"
+	//}
 
 	dbName := os.Getenv("DATABASE_NAME")
-	if dbName == "" {
-		// Set a default value for local development
-		dbName = "edms"
-	}
+	//if dbName == "" {
+	//	// Set a default value for local development
+	//	dbName = "edms"
+	//}
 
 	dbPort := os.Getenv("DATABASE_PORT")
-	if dbPort == "" {
-		// Set a default value for local development
-		dbPort = "5432" // Assuming the default PostgreSQL port is 5432
-	}
+	//if dbPort == "" {
+	//	// Set a default value for local development
+	//	dbPort = "5432" // Assuming the default PostgreSQL port is 5432
+	//}
 
 	// Construct the database connection string
 	dsn := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", dbHost, dbUser, dbName, dbPassword, dbPort)
